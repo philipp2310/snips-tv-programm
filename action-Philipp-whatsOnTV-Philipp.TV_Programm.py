5952f528a9b825da27db06c50c0ee1c68fcb3e83
Philipp <41761223+philipp2310@users.noreply.github.com>
2018-09-27 17:44:17 +0200

    import2...

--- action-Philipp-whatsOnTV-Philipp.TV_Programm.py
+++ action-Philipp-whatsOnTV-Philipp.TV_Programm.py
@@ -6,6 +6,7 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 import urllib
+import xmltodict
 
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"

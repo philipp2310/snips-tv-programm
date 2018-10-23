7037d4103978a19bc66fed576eb6807d8c3a6055
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2018-10-23 22:15:54 +0200

    update 1

--- snips_webView.py
+++ snips_webView.py
@@ -3,20 +3,37 @@
 
 import io
 import sys
-# 
+import paho.mqtt.publish as publish
+
+# webView loads a html file and creates a single output line that can be transfered via MQTT
 class webView:
-    def __init__(self, listName, callName):
-        self.list_path = listName
-        self.callName  = callName
-        self.storeList = self.read_storeList()
+    def __init__(self, htmlFile, appName, sideID='default'):
+        self.appName = appName
+        self.htmlFile = htmlFile
+        self.html = ""
+        self.topic = "Wilma/disp/"+self.sideID+"/"+appName
         # set encoding for umlauts...
         reload(sys)
         sys.setdefaultencoding('utf-8')
 
-        return response
-
     def read_file(self):
         try:
-            file = io.open(self.list_path, 'rb')
+            self.html = io.open(self.htmlFile, 'rb')
         except EOFError:
             return []
+
+# <style type="text/css">
+#//CSS goes here
+#</style>
+#<script type="text/javascript">
+#//Javascript goes here
+#</script>
+    def insert_includes():
+        self.html = self.html
+        
+    def insert_data(self, field, data):
+        self.html = self.html.replace('{{'+field+'}}',data,1)
+        
+    def send_to_display():
+        publish(self.topic, self.html, hostname="localhost", port=1883)
+        
